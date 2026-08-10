0d80100a1f1a688f9e9b4d400cf1c0633e55f674
arun95tech <arun95.tech@gmail.com>
2026-08-10 18:58:07 +0100

    update admin.py

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StockLocation, StockLedger, StockAdjustment
 
-# Register your models here.
+admin.site.register(StockLocation)
+admin.site.register(StockLedger)
+admin.site.register(StockAdjustment)
